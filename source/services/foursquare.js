9835a22304e1ca106dfed3185984dedebdc05a46
Artem <artem@wearebrighter.com>
2018-05-01 15:52:48 +0300

    Add foursquare api

---
+++ source/services/foursquare.js
@@ -0,0 +1,48 @@
+import queryString from 'query-string'
+
+import { FOURSQUARE_CLIENT_ID, FOURSQUARE_CLIENT_SECRET } from '../config'
+
+const apiUrl = 'https://api.foursquare.com/v2/venues'
+const version = '20171001'
+
+const defaultParams = {
+  client_id: FOURSQUARE_CLIENT_ID,
+  client_secret: FOURSQUARE_CLIENT_SECRET,
+  v: version
+}
+
+const request = urlString => {
+  return new Promise((resolve, reject) => {
+    fetch(urlString).then(response => response.json()).then(response => {
+      if (response.meta.code === 200) {
+        resolve(response.response)
+      } else {
+        reject(new Error(
+          `Code ${response.meta.code}; Message: ${response.meta.errorDetail}`))
+      }
+    }).catch(error => {
+      reject(error)
+    })
+  })
+}
+
+export const fetchPlaces = () => {
+  let params = {
+    query: 'vegan',
+    intent: 'global'
+  }
+  params = Object.assign(params, defaultParams)
+
+  let urlString = `${apiUrl}/search?${queryString.stringify(params)}`
+
+  return request(urlString).then(response => response.venues)
+}
+
+export const fetchPlace = id => {
+  let params = {}
+  params = Object.assign(params, defaultParams)
+
+  let urlString = `${apiUrl}/${id}?${queryString.stringify(params)}`
+
+  return request(urlString).then(response => response.venue)
+}
